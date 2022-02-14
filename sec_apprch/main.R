7feb255a27ae7cbfa5fc55cfb5b311b850f32176
ChenYenRu <rebecca946487@gmail.com>
2022-02-14 20:06:26 +0800

    update

--- sec_apprch/main.R
+++ sec_apprch/main.R
@@ -10,36 +10,50 @@ revenue <- read_csv("~/Documents/GitHub/Data_Analysis/air_qu/sec_apprch/DATA/rev
 colnames(doctors)[1] <- "year"
 doctors <- doctors %>%
   gather("county", "doctor", -1)
+doctors <- filter(doctors, county == "臺中市")
 # doctors <- doctors[, c("year", "臺中市")]
 # colnames(doctors)[2] <- "doctor"
 
 colnames(ozone)[1] <- "year"
 ozone <- ozone %>%
   gather("county", "ozone", -1)
+ozone <- filter(ozone, county == "臺中市")
 # ozone <- ozone[, c("year", "臺中市")]
 # colnames(ozone)[2] <- "ozone"
 
 colnames(revenue)[1] <- "year"
 revenue <- revenue %>%
   gather("county", "revenue", -1)
+revenue <- filter(revenue, county == "臺中市")
 # revenue <- revenue[, c("year", "臺中市")]
 # colnames(revenue)[2] <- "revenue"
 
+lung <- read_csv("~/Documents/GitHub/Data_Analysis/air_qu/sec_apprch/DATA/deaths_lung.csv")
+lung <- head(lung, -2)
+lung <- lung %>%
+  gather("sex", "lung_cancer", 3:4)
+
+lung
+colnames(lung)[1] <- "year"
+colnames(lung)[2] <- "county"
+lung$sex = NULL
 
 doctors
 ozone
 revenue
 
 merge_one <- merge(doctors, ozone, by=c(1, 2))
-View(merge_one)
+# View(merge_one)
 
 merge_two<- merge(merge_one, revenue, by=c(1, 2))
 colnames(merge_two)[1] <- "year"
-View(merge_two)
+# View(merge_two)
 
+merge_three <- merge(merge_two, lung, by=c(1, 2))
+# View(merge_three)
 
 Sys.setlocale("LC_ALL","Chinese")
-write.csv(merge_two, "DATA/merged.csv")
+write.csv(merge_three, "DATA/merged.csv")
 
 # Importing the large dataset of all voila
 
@@ -79,15 +93,17 @@ ggplot(file, aes(x = doctor, y = ozone))+
   geom_smooth(method=lm)
 
 # Fixed Effects Model
+library(car)
 fixed.dum <-lm(ozone ~ doctor + factor(county) - 1, data = file)
 summary(fixed.dum)
-
-
 yhat <- fixed.dum$fitted
 scatterplot(yhat ~ file$doctor | file$county,  xlab ="doctor", ylab ="yhat of ozone", boxplots = FALSE,smooth = FALSE)
-abline(lm(dataPanel101$y~dataPanel101$x1),lwd=3, col="red")            
+abline(lm(file$y~file$doctor),lwd=3, col="red")    
 
 
+# Random Effects Model
+random <- plm(ozone ~ doctor, data=file, model="random")
+summary(random)
 
 
 ## Using PLM
@@ -97,3 +113,8 @@ pdf = pdata.frame(file, index = c("county", "year"))
 # specify and run the model
 model = plm(ozone~doctor, data = pdf, model = "random")
 summary(model)
+
+
+
+# Decide using fixed or random
+phtest(fixed, random)
